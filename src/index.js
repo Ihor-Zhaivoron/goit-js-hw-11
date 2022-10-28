cc90444e00fca9aec1c7633b43e5c1c03ae18168
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2022-10-29 00:25:58 +0300

    git commit -am 'error way temlates"

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
 
-axios.get('/users').then(res => {
-  console.log(res.data);
-});
+import { fetchApi } from './js/fetchImagesApi';
+import cards from './templates/cards.hbs';
+// import '..css/style.css';
+import './css/style.css';
+
+const input = document.querySelector('input');
+const searchForm = document.querySelector('#search-form');
+const loadMoreBtn = document.querySelector('.load-more');
+
+searchForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+}
--- src/js/fetchImagesApi.js
+++ src/js/fetchImagesApi.js
@@ -1,2 +1,11 @@
-const url = 'https://pixabay.com/api';
-const key = '30885562 - a2eefab65b39820f24403263b';
+const axios = require('axios').default;
+
+const BASE_URL = 'https://pixabay.com/api';
+const KEY = '30885562 - a2eefab65b39820f24403263b';
+
+export async function fetchImages() {
+  const response = await axios(`${BASE_URL}?key=${KEY}&;.....`);
+
+  console.log(response.data);
+  return response.data;
+}
