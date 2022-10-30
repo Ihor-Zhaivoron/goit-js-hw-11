9b66870b4909f56bee5b0d215303fb5d395ca03e
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2022-10-30 22:36:16 +0200

    finish stylesl

--- src/js/fetchImagesApi.js
+++ src/js/fetchImagesApi.js
@@ -1,5 +1,5 @@
-// const axios = require('axios').default;
-import axios from 'axios';
+const axios = require('axios').default;
+// import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '30885562-a2eefab65b39820f24403263b';
 //  const otherParams = {
@@ -15,6 +15,6 @@ export async function fetchImages(searchQuery, page) {
   const response = await axios.get(
     `${BASE_URL}?key=${KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
   );
-  console.log(response.data);
+  // console.log(response.data);
   return response.data;
 }
