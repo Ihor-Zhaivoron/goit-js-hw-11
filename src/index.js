c95ee7f070bb21f1c2c3ce53a65a5515e7f88784
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2022-10-30 12:10:33 +0200

    test commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
-import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
 
 import { fetchApi } from './js/fetchImagesApi';
 import cards from './templates/cards.hbs';
-// import '..css/style.css';
+
 import './css/style.css';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const input = document.querySelector('input');
 const searchForm = document.querySelector('#search-form');
 const loadMoreBtn = document.querySelector('.load-more');
+const searchBtn = document.querySelector('button[type="submit"]');
+const gallery = document.querySelector('.gallery');
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 searchForm.addEventListener('submit', onSubmit);
+loadMoreBtn.addEventListener('click', onLoadMore);
+loadMoreBtn.style.display = 'none';
 
 function onSubmit(e) {
   e.preventDefault();
+  cleanImages();
+  const searchValue = e.target.input.value.trim();
+
+  if (searchValue) {
+  }
 }
--- src/js/fetchImagesApi.js
+++ src/js/fetchImagesApi.js
@@ -1,11 +1,20 @@
-const axios = require('axios').default;
+const axios = require('axios').default; //import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api';
 const KEY = '30885562 - a2eefab65b39820f24403263b';
+// const otherParams = {
+//   KEY:'30885562 - a2eefab65b39820f24403263b',
+//   q: searchQuery,
+//   image_type: 'photo',
+//   orientation: 'horizontal',
+//   safesearch: true,
+//   per_page,
+//   page,
+// };
 
 export async function fetchImages() {
-  const response = await axios(`${BASE_URL}?key=${KEY}&;.....`);
-
-  console.log(response.data);
-  return response.data;
+  const response = await axios(
+    `${BASE_URL}?key=${KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
+  );
+  return response;
 }
